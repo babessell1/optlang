1bca6f5e6160a0a313d80ad1456240631b9a08b2
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-07-26 15:18:52 -0400

    Update interface.py

--- src/optlang/interface.py
+++ src/optlang/interface.py
@@ -966,7 +966,7 @@ class Objective(OptimizationExpression):
             expression,
             direction=json_obj["direction"],
             name=json_obj["name"]
-        
+        )
 
 class Configuration(object):
     """
